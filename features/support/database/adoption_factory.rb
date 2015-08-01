5c29686542417ac48790d3b6ff4bbdadd8b0e377
Dave Huff <David.Huff@computer-critters.com>
2015-07-31 23:22:52 -0400

    Utilized Faker added populator

--- features/support/database/adoption_factory.rb
+++ features/support/database/adoption_factory.rb
@@ -1,10 +1,18 @@
 require_relative 'puppy'
 
+# FactoryGirl.define do
+#   factory :order do
+#     name 'Cheezy'
+#     address '123 Main'
+#     email 'cheezy@me.com'
+#     pay_type 'Check'
+#   end
+
 FactoryGirl.define do
   factory :order do
-    name 'Cheezy'
-    address '123 Main'
-    email 'cheezy@me.com'
+    name Faker::Name.name
+    address Faker::Address.street_address
+    email Faker::Internet.email
     pay_type 'Check'
   end
 
