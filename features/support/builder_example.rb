96d411afa507b7c0bfca0fd83c4613278d3d0e8d
Dave Huff <David.Huff@computer-critters.com>
2015-08-04 21:45:56 -0400

    refactor builder example

--- features/support/builder_example.rb
+++ features/support/builder_example.rb
@@ -1,30 +1,16 @@
 require 'builder'
 require 'faker'
 
-# builder = Builder::XmlMarkup.new(:target=>STDOUT, :indent=>2)
-file = File.new('name_and_phone.xml', 'wb')
-builder = Builder::XmlMarkup.new :target => file
+# builder = Builder::XmlTree.new(:target=>STDOUT, :indent=>2)
+file = File.new('../../name_and_phone.xml', 'wb')
+builder = Builder::XmlMarkup.new :target => file, :indent => 2
 
 xml = builder.contacts do
-  xml = builder.contact do |c|
-    c.name Faker::Name.name
-    c.phone Faker::PhoneNumber.phone_number
-  end
-  xml = builder.contact do |c|
-    c.name Faker::Name.name
-    c.phone Faker::PhoneNumber.phone_number
-  end
-  xml = builder.contact do |c|
-    c.name Faker::Name.name
-    c.phone Faker::PhoneNumber.phone_number
-  end
-  xml = builder.contact do |c|
-    c.name Faker::Name.name
-    c.phone Faker::PhoneNumber.phone_number
-  end
-  xml = builder.contact do |c|
-    c.name Faker::Name.name
-    c.phone Faker::PhoneNumber.phone_number
+  50.times do
+    xml = builder.contact do |c|
+      c.name Faker::Name.name
+      c.phone Faker::PhoneNumber.phone_number
+    end
   end
 end
 
