20fdcc2ee0f6cf36048e4a65b4fe928ef954e996
Dave Huff <David.Huff@computer-critters.com>
2015-09-04 11:55:26 -0400

    pretty_face working all tests green

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -7,6 +7,12 @@ Before do
 end
 
 
-After do
+After do |scenario|
+  if scenario.failed?
+    time = Time.now.strftime("%Y-%m-%d_%H-%m-%S")
+    filename = "error-#{ @current_page.class }-#{ time }.png"
+    @current_page.save_screenshot(filename)
+    embed(filename, image/png)
+  end
   @browser.close
 end
